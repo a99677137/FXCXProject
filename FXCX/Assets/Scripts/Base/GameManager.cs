f3b582bb0156b9dac1b27f293fa9fa3875ab755e
a99677137 <99677137@qq.com>
2018-03-01 10:14:10 +0800

    添加CommonTools的dll~和UIRoot

---
+++ FXCX/Assets/Scripts/Base/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Tools.CommonTools;
+
+public class GameManager : MonoBehaviour {
+
+
+    #region MonoBehaviour
+    // Use this for initialization
+    void Start () {
+        GameLog.Debug("---------------------------------------GameStart-----------------------------------------");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+    #region GUI
+
+    #if GAMEDEBUG
+    void OnGUI()
+    {
+        
+    }
+
+#endif //GAMEDEBUG
+
+
+
+    #endregion //GUI
+
+
+    #endregion //MonoBehaviour
+
+    #region GameLogic
+
+    
+
+    #endregion
+}
