43223a2e9e324f2c888f89a0721abd06b1d4d8d6
a99677137 <99677137@qq.com>
2018-06-08 11:54:21 +0800

    添加config文件的加载，以及导出工程时部分处理

---
+++ FXCX/Assets/Scripts/Base/Config/ResourceBase.cs
@@ -0,0 +1,45 @@
+﻿using Game.Tools.CommonTools;
+using UnityEngine;
+using System.IO;
+using System;
+
+namespace Game.Lwn.Base
+{
+    public class ResourceBase : Singleton<ResourceBase>
+    {
+
+        private string GetEditorCFGPath()
+        {
+            return Application.dataPath + "/../GameAssets/Config/";
+        }
+
+        private string GetEditorLuaPath()
+        {
+            return Application.dataPath + "/../GameAssets/Lua/";
+        }
+
+
+
+        public string LoadConfigFile(string path)
+        {
+            string fullpath = "";
+            string result = "";
+#if UNITY_EDITOR
+            fullpath = GetEditorCFGPath() + path;
+            GameLog.Debug("ResourceBase:LoadConfigFile----fullpath = {0}", fullpath);
+            byte[] data = System.IO.File.ReadAllBytes(fullpath);
+            if (data == null) {
+                GameLog.Error("ERROR!!!----ResourceBase:LoadConfigFile----path = {0}", path);
+                return "";
+            }
+            byte[] byteArr = CusEncoding.EncodingUtil.FileByteToLocal(data);
+            result = new String(System.Text.Encoding.UTF8.GetChars(byteArr));
+#else
+
+
+
+#endif
+            return result;
+        }
+    }
+}
---
+++ FXCX/Assets/Scripts/Base/Editor/FXCX_PostProcess.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using Game.Tools.CommonTools;
+using System.IO;
+using UnityEngine;
+
+public class FXCX_PostProcess
+{
+
+#if UNITY_EDITOR
+    [PostProcessBuild(100)]
+    public static void OnFxcxPostProcessBuild(BuildTarget target, string pathToBuiltProject) {
+        if (BuildTarget.Android == target) {
+            GameLog.Debug("<color=#aa0099>--------------FXCX_PostProcess:OnFxcxPostProcessBuild----Target = Android---------</color>");
+            GameLog.Debug("<color=#aa0099>--------------FXCX_PostProcess:OnFxcxPostProcessBuild----pathToBuiltProject = " + pathToBuiltProject + "---------</color>");
+            //PostProcessAndroidBuild(pathToBuiltProject);
+        } else if (BuildTarget.iOS == target) {
+            GameLog.Debug("<color=#aa0099>--------------FXCX_PostProcess:OnFxcxPostProcessBuild----Target = iOS---------</color>");
+            GameLog.Debug("<color=#aa0099>--------------FXCX_PostProcess:OnFxcxPostProcessBuild----pathToBuiltProject = "+ pathToBuiltProject + "---------</color>");
+        }
+    }
+#endif
+
+
+    public static void PostProcessAndroidBuild(string pathToBuiltProject)
+    {
+        ScriptingImplementation backend = PlayerSettings.GetScriptingBackend(BuildTargetGroup.Android);
+
+        if (backend == ScriptingImplementation.IL2CPP)
+        {
+            CopyAndroidIL2CPPSymbols(pathToBuiltProject, PlayerSettings.Android.targetDevice);
+        }
+    }
+
+    public static void CopyAndroidIL2CPPSymbols(string pathToBuiltProject, AndroidTargetDevice targetDevice)
+    {
+        string buildName = Path.GetFileNameWithoutExtension(pathToBuiltProject);
+        FileInfo fileInfo = new FileInfo(pathToBuiltProject);
+        string symbolsDir = fileInfo.Directory.Name;
+        symbolsDir = symbolsDir + "/" + buildName + "_IL2CPPSymbols";
+
+        CreateDir(symbolsDir);
+
+        switch (PlayerSettings.Android.targetDevice)
+        {
+            case AndroidTargetDevice.FAT:
+                {
+                    CopyARMSymbols(symbolsDir);
+                    CopyX86Symbols(symbolsDir);
+                    break;
+                }
+            case AndroidTargetDevice.ARMv7:
+                {
+                    CopyARMSymbols(symbolsDir);
+                    break;
+                }
+            case AndroidTargetDevice.x86:
+                {
+                    CopyX86Symbols(symbolsDir);
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+
+    const string libpath = "/../Temp/StagingArea/libs/";
+    const string libFilename = "libil2cpp.so.debug";
+    private static void CopyARMSymbols(string symbolsDir)
+    {
+        string sourcefileARM = Application.dataPath + libpath + "armeabi-v7a/" + libFilename;
+        CreateDir(symbolsDir + "/armeabi-v7a/");
+        File.Copy(sourcefileARM, symbolsDir + "/armeabi-v7a/libil2cpp.so.debug");
+    }
+
+    private static void CopyX86Symbols(string symbolsDir)
+    {
+        string sourcefileX86 = Application.dataPath + libpath + "x86/libil2cpp.so.debug";
+        File.Copy(sourcefileX86, symbolsDir + "/x86/libil2cpp.so.debug");
+    }
+
+    public static void CreateDir(string path)
+    {
+        if (Directory.Exists(path))
+            return;
+
+        Directory.CreateDirectory(path);
+    }
+
+}
